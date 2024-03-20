5e137c310f0183b11e2b5a22a38ff95e342c0aec
Da-RA-21 <rashawnthompson@unomaha.edu>
2024-03-19 20:52:05 -0500

    Added code to implement the ability to view the contents of my to-do list table in my database. This included adding a model, passing in the table contents as 'data' in view.py, then implementing a for loop to look at each row of the table one by one.

--- techExercise/models.py
+++ techExercise/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class TodoList(models.Model):
+    item = models.CharField(max_length=255, blank=True, null=True)
+    itemid = models.AutoField(db_column='itemID', primary_key=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'todo_list'
+
 # Create your models here.
