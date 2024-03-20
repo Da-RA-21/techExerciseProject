82d28fc1ea4bfb487a03ff61760e5bed015d633c
Da-RA-21 <rashawnthompson@unomaha.edu>
2024-03-19 20:20:28 -0500

    Connected my project to the mysql database hosted on my development computer (my laptop), named "techexercise". Removed the sqlite3 file from the project.

--- techExerciseProject/settings.py
+++ techExerciseProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'techExerciseProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'techexercise',
+        'USER': 'rathompson',
+        'PASSWORD': '$Orion1$',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
